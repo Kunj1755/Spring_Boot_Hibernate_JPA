948430c5ee5d22dd468746f09af8ffe46a25d4ee
Kunj Bihari <kunjbihari1756@gmail.com>
2018-09-05 10:02:01 +0100

    Step 04 - Populate data into Person Table

--- database-demo/src/main/resources/data.sql
+++ database-demo/src/main/resources/data.sql
@@ -7,3 +7,6 @@ create table person
    primary key(id)
 );
 
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10001,  'Ranga', 'Hyderabad',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10002,  'James', 'New York',sysdate());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE ) VALUES(10003,  'Pieter', 'Amsterdam',sysdate());
