36b0677bd5a32aecf8595604773168e9c5929420
Kunj Bihari <kunjbihari1756@gmail.com>
2018-09-06 09:03:51 +0100

    advanced-jpa module added

---
+++ advanced-jpa/src/main/java/com/personal/Kunj/jpa/advancedjpa/AdvancedJpaApplication.java
@@ -0,0 +1,12 @@
+package com.personal.Kunj.jpa.advancedjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedJpaApplication.class, args);
+	}
+}
