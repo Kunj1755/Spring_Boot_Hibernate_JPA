337cffe0e52cb1259b1b2a093ef9856115346c13
Kunj Bihari <kunjbihari1756@gmail.com>
2018-09-06 09:55:22 +0100

    Step 02 - Create JPA Entity Course

---
+++ advanced-jpa/src/main/java/com/personal/Kunj/jpa/advancedjpa/entity/Course.java
@@ -0,0 +1,35 @@
+package com.personal.Kunj.jpa.advancedjpa.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+
+	// To define primary key
+	@Id
+	@GeneratedValue // want JPA to generate for us
+	private Long id;
+
+	private String name;
+	// Default constructor will be used by JPA to create bean
+	protected Course() {
+	}
+
+	// WE want others only to provide the name , not id
+	public Course(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+}
