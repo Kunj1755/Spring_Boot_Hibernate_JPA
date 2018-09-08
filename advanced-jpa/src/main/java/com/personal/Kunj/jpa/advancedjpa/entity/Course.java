d5dab32c241dac963fa914dad98a1f152c7c643b
Kunj Bihari <kunjbihari1756@gmail.com>
2018-09-08 19:09:21 +0100

    Step 22 - Defining Entities - Student, Passport and Review

--- advanced-jpa/src/main/java/com/personal/Kunj/jpa/advancedjpa/entity/Course.java
+++ advanced-jpa/src/main/java/com/personal/Kunj/jpa/advancedjpa/entity/Course.java
@@ -13,23 +13,12 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
-// To define the name of the table
-@Table(name="CourseDetails")
-// we can either use multiple @NamedQuery or @NamedQueries
-
-@NamedQueries(value = { @NamedQuery(name = "query_get_all_courses", query = "Select  c  From Course c"),
-		@NamedQuery(name = "query_get_100_step_courses", query = "Select  c  From Course c where name like '%100 Steps'") })
-
-/*@NamedQuery(name="query_get_all_courses", query="Select  c  From Course c")
-@NamedQuery(name="query_get_100_step_courses", query="Select  c  From Course c where name like '%100 Steps'")*/
 public class Course {
 
-	// To define primary key
 	@Id
-	@GeneratedValue // want JPA to generate for us
+	@GeneratedValue 
 	private Long id;
 
-	// Set all the constraints here that you have in your data in dB to prevent bad data from entering into DB
 	@Column(name="fullname", nullable=false)
 	private String name;
 	
