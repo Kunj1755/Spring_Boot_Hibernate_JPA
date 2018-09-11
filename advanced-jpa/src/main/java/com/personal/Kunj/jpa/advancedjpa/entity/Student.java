529e61a6a2ccb77d1516b67ff21fb2ab61230f35
Kunj Bihari <kunjbihari1756@gmail.com>
2018-09-11 10:54:14 +0100

    Step 37 - ManyToMany Mapping - Customizing the Join Table

--- advanced-jpa/src/main/java/com/personal/Kunj/jpa/advancedjpa/entity/Student.java
+++ advanced-jpa/src/main/java/com/personal/Kunj/jpa/advancedjpa/entity/Student.java
@@ -1,4 +1,5 @@
 package com.personal.Kunj.jpa.advancedjpa.entity;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
@@ -19,16 +22,23 @@ public class Student {
 
 	@Column(nullable = false)
 	private String name;
-	
-	@OneToOne(fetch=FetchType.LAZY) 
+
+	@OneToOne(fetch = FetchType.LAZY)
 	private Passport passport;
-	
+
 	@ManyToMany
+	// JoinColumn for this entity is Student_id.
+	// InverseJoinColumn is course_id
+	@JoinTable(name = "STUDENT_COURSE", 
+	joinColumns = @JoinColumn(name = "STUDENT_ID"),
+	inverseJoinColumns = @JoinColumn(name = "COURSE_ID")
+	)
+
 	private List<Course> courses = new ArrayList<>();
 
 	protected Student() {
 	}
-	
+
 	public List<Course> getCourses() {
 		return courses;
 	}
